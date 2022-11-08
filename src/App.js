cb29b9b31978598d50bff3be5b5b97573c5af116
andres lopez <andres.lopez.yepez.28@gmail.com>
2022-11-08 15:48:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Lobby from './components/Lobby';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import { useState } from 'react';
+import Chat from './components/Chat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [connection, setConnection] = useState();
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  const joinRoom = async (user, room) => {
+    try{
+      const connection = new HubConnectionBuilder()
+      .withUrl("https://localhost:7213/chat")
+      .configureLogging(LogLevel.Information)
+      .build();
+
+      connection.on("ReceiveMessage", (user, message) => {
+        setMessages(messages => [...messages, {user, message}]);
+        console.log('message received: ', message);
+      })
+
+      connection.on("UsersInRoom", (users) => {
+        setUsers(users);
+      });
+
+      connection.onclose(e => {
+        setConnection();
+        setMessages([]);
+        setUsers([]);
+      });      
+
+      await connection.start();
+      await connection.invoke("JoinRoom", {user, room});
+      setConnection(connection);
+
+    } catch(e)
+    {
+      console.log({e});
+    }
+  }
+
+  const sendMessage = async (message) => {
+    try {
+      await connection.invoke("SendMessage", message);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const closeConnection = async () => {
+    try {
+      await connection.stop();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return <div className='app'>
+    <h2>Chat Service</h2>
+    <hr className='line' />
+    {
+      !connection ? <Lobby joinRoom={joinRoom} /> 
+      : <Chat sendMessage={sendMessage} messages={messages}
+              closeConnection={closeConnection} 
+              users={users} />
+    }
+  </div>
 }
 
 export default App;
